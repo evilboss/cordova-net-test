50cb077d440e122d9af5b00bd15dd1bbedb0cd3d
Jr Reyes <jr.evilboss@gmail.com>
2016-03-21 15:51:41 +0800

    Added new packages

--- www/js/app.js
+++ www/js/app.js
@@ -1,6 +1,32 @@
 /**
- * Created by gilbertor on 2/12/16.
+ * Created by gilbertor on 3/17/16.
  */
-Meteor.startup(function () {
+function closeApp() {
+  navigator.app.exitApp();
+}
+function updateUi() {
+  intervalID = setInterval(function () {
+      getRequest();
+  }, 5000);
+}
+function getRequest() {
+  var url = "https://toolbox.cloudstaff.com/~noc-display/test.txt";
+  cordovaHTTP.setHeader("Access-Control-Allow-Origin", "*");
+  var result;
+  cordovaHTTP.get(url, {}, {}, function (response) {
+    $('#httpResult').html(response.data);
+  }, function (response) {
+    $('#httpResult').html(response.error);
+  });
 
-});
+}
+function ping(){
+
+}
+function init() {
+  $("#exit-app").click(function () {
+    closeApp();
+  });
+  updateUi();
+}
+document.addEventListener("deviceready", init, false);
