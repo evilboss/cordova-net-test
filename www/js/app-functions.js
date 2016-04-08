db7432ecfa44cb2b9def2452fc3b1499d5e85a2c
Jr Reyes <jr.evilboss@gmail.com>
2016-04-08 12:21:20 +0800

    Added new function getUrl and ping

--- www/js/app-functions.js
+++ www/js/app-functions.js
@@ -2,8 +2,24 @@
  * Created by gilbertor on 3/31/16.
  */
 var connected = false;
-var dnsDone = false;
+var httpDone = false;
 var pingDone = false;
+function getSettingsUrl() {
+  var url = "https://toolbox.cloudstaff.com/~noc-display/test.txt";
+  var settingsUrl = window.localStorage.getItem("url");
+  if (settingsUrl) {
+    url = settingsUrl;
+  }
+  return url;
+}
+function getPingTarget() {
+  var pingTarget = '8.8.8.8';
+  var settingsPingTarget = window.localStorage.getItem("ping-target");
+  if (settingsPingTarget) {
+    pingTarget = settingsPingTarget;
+  }
+  return pingTarget;
+}
 function setConnectionState(connectionType) {
   if (connectionType === 'WiFi connection') {
     return 'led-green'
@@ -35,48 +51,37 @@ function closeApp() {
   navigator.app.exitApp();
 }
 function getRequest() {
-  if (!connected) {
-    var url = "https://toolbox.cloudstaff.com/~noc-display/test.txt";
-    var settingsUrl = window.localStorage.getItem("url");
-    if (settingsUrl) {
-      url = settingsUrl;
-    }
-    cordovaHTTP.setHeader("Access-Control-Allow-Origin", "*");
-    cordovaHTTP.get(url, {}, {}, function (response) {
-      $('#httpResult').html(response.data);
-      $('#httpLed').html('<div class="led-green"></div>');
-      $('#isConnected').html('<span class="green-text">Connected</span>');
-      networkinterface.getIPAddress(function (ip) {
-        $('#ip-address').html(ip);
-      });
-      window.MacAddress.getMacAddress(
-        function (macAddress) {
-          $('#mac-address').html(macAddress);
-        }, function (fail) {
-          $('#mac-address').html(fail);
-        }
-      );
-      $('#network-status').html('On-Line');
-      connected = true;
-    }, function (response) {
-
-      $('#httpResult').html(response.error);
-      $('#httpLed').html('<div class="led-red"></div>');
-      $('#isConnected').html('<span class="red-text">Please connect to the internet</span>');
-      connected = false;
-      if (response.status) {
-        if (response.status == '302') {
-          $('#isConnected').html('<span class="red-text">Request Has Been redirected to a different url</span>');
-        }
-        connected = true;
-      }
+  var url = getSettingsUrl();
+  cordovaHTTP.setHeader("Access-Control-Allow-Origin", "*");
+  cordovaHTTP.get(url, {}, {}, function (response) {
+    $('#httpResult').html(response.data);
+    $('#httpLed').html('<div class="led-green"></div>');
+    $('#isConnected').html('<span class="green-text">Connected</span>');
+    networkinterface.getIPAddress(function (ip) {
+      $('#ip-address').html(ip);
     });
-  }
+    window.MacAddress.getMacAddress(
+      function (macAddress) {
+        $('#mac-address').html(macAddress);
+      }, function (fail) {
+        $('#mac-address').html(fail);
+      }
+    );
+    $('#network-status').html('On-Line');
+  }, function (response) {
+    $('#httpResult').html(response.error);
+    $('#httpLed').html('<div class="led-red"></div>');
+    $('#isConnected').html('<span class="red-text">Please connect to the internet</span>');
+    if (response.status) {
+      if (response.status == '302') {
+        $('#isConnected').html('<span class="red-text">Request Has Been redirected to a different url</span>');
+      }
+    }
+  });
 }
 function makeid() {
   var text = "";
   var possible = "abcdefghijklmnopqrstuvwxyz";
-
   for (var i = 0; i < 5; i++)
     text += possible.charAt(Math.floor(Math.random() * possible.length));
   return text;
@@ -87,58 +92,33 @@ function getDns(url) {
       $('#led-dns').html('<div class="led-green"></div>');
       $('#status-dns').html('<strong class="green-text text-darken-3">Resolved</strong>');
       $('#dns').html('<strong class="green-text text-darken-3">Resolved</strong>');
+      connected = true;
+
     }
     $('#ip-dns').html('<strong class="green-text text-darken-3">' + ip + '</strong>');
     $('#target-dns').html('<strong class="green-text text-darken-3">' + url + '</strong>');
-
-
   });
-}
-function updateUi() {
-  intervalID = setInterval(function () {
-    checkConnection();
-    getRequest();
-    if (connected) {
-      if (!pingDone) {
-        var pingTarget = '8.8.8.8';
-        var settingsPingTarget = window.localStorage.getItem("ping-target");
-        if (settingsPingTarget) {
-          pingTarget = settingsPingTarget;
-        }
-        ping(pingTarget, 'ping');
-        pingDone = true;
-      }
-      if (!dnsDone) {
-        getDns(makeid() + '.cloudstaff.io');
-        dnsDone = true;
-      }
-    }
-  }, 100);
 }
 function setLed(target, result) {
   var speed = '<strong class="yellow-text text-darken-3">Slow</strong>';
   if (result) {
     result = parseInt(result);
-    if (result < 50) {
+    if (result < 75) {
       speed = '<strong class="green-text">Fast</strong>';
       $('#led-' + target).html('<div class="led-green"></div>');
-
     }
     if (result < 100) {
       speed = '<strong class="blue-text">OK</strong>';
       $('#led-' + target).html('<div class="led-blue"></div>');
-
     }
-    if (result < 100) {
+    if (result < 200) {
       speed = '<strong class="yellow-text text-darken-3">Slow</strong>';
       $('#led-' + target).html('<div class="led-yellow"></div>');
-
     }
     $('#' + target).html(speed);
   } else {
     $('#' + target).html('<strong class="red-text text-darken-3">Timeout</strong>');
   }
-
 }
 function ping(url, target) {
   var p, success, err, ipList;
@@ -150,8 +130,6 @@ function ping(url, target) {
       $('#status-' + target).html(results[0].status);
       setLed(target, results[0].avg)
     }
-
-
   };
   err = function (e) {
     $('#' + target).html(e);
@@ -164,4 +142,26 @@ function initializeApp() {
   $('#exit-app').click(function () {
     closeApp();
   });
+}
+function updateUi() {
+  intervalID = setInterval(function () {
+    checkConnection();
+    if (!connected) {
+      getDns(makeid() + '.cloudstaff.io');
+    }
+
+
+    if (connected) {
+      if (!pingDone) {
+        var pingTarget = getPingTarget();
+        ping(pingTarget, 'ping');
+        pingDone = true;
+      }
+      if (!httpDone) {
+        getRequest();
+
+        httpDone = true;
+      }
+    }
+  }, 100);
 }
